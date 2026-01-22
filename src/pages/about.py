eba16b0c82d740e82b3a7f81c43f0db4408688f7
thais-castillo <thais.castillo@edu.esiee.fr>
2026-01-22 10:39:26 +0100

    [FEAT]:add begining page about

--- src/pages/about.py
+++ src/pages/about.py
@@ -11,13 +11,21 @@ def layout():
                 style={
                     "--color": "var(--pink)",
                     "--top": "60px",
-                    "--right": "550px"
+                    "--right": "450px"
                 }
             ),
             html.Div(
                 className="background-glow",
                 style={
                     "--color": "var(--pink)",
+                    "--top": "500px",
+                    "--right": "450px"
+                }
+            ),
+            html.Div(
+                className="background-glow",
+                style={
+                    "--color": "var(--blue)",
                     "--top": "350px",
                     "--left": "50px"
                 }
@@ -26,8 +34,8 @@ def layout():
                 className="background-glow",
                 style={
                     "--color": "var(--blue)",
-                    "--top": "500px",
-                    "--right": "450px"
+                    "--top": "50px",
+                    "--right": "-20px"
                 }
             ),
             # Gros conteneur de texte
@@ -43,7 +51,8 @@ def layout():
                             className="main-text-container",
                             children=[
                                 html.H1("About us..."),
-                                html.P("Votre texte principal ici. Ce conteneur peut contenir autant de texte que nécessaire.")
+                                html.P("Ce Dashboard a été réalisé dans le cadre d'un projet académique pour l'école d'ingénieure ESIEE Paris dans la filière en apprentissage Informatique et Applications, "
+                                "par Clémence Carcanade, apprentie chez Dassault Système et Thaïs Castillo, apprentie chez Crédit Agricole Payment Services.")
                             ]
                         ),
                         # Petits conteneurs alignés en dessous
@@ -52,9 +61,39 @@ def layout():
             html.Div(
                             className="cards-row",
                             children=[
-                                create_card(html.P("Texte 1")),
-                                create_card(html.P("Texte 2")),
-                                create_card(html.P("Texte 3")),  # Vous pouvez en ajouter plus
+                                create_card(
+                                    html.P([
+                                        html.Strong("Objectifs du projet :"),
+                                        html.Br(),
+                                        "Texte normal ligne 1",
+                                        html.Br(),
+                                        "Texte normal ligne 2",
+                                        html.Br(),
+                                        "Texte normal ligne 3"
+                                    ])
+                                ),
+                                create_card(
+                                    html.P([
+                                        html.Strong("Outils utilisés :"),
+                                        html.Br(),
+                                        "Texte normal ligne 1",
+                                        html.Br(),
+                                        "Texte normal ligne 2",
+                                        html.Br(),
+                                        "Texte normal ligne 3"
+                                    ])
+                                ),
+                                create_card(
+                                    html.P([
+                                        html.Strong("Pourquoi ce sujet ?"),
+                                        html.Br(),
+                                        "Texte normal ligne 1",
+                                        html.Br(),
+                                        "Texte normal ligne 2",
+                                        html.Br(),
+                                        "Texte normal ligne 3"
+                                    ])
+                                )
                             ]
                         )   
         ]
