676cd34aa7433a423a2d389428ff215068e23cc4
clemence-carcanade <clemence.carcanade@edu.esiee.fr>
2026-01-31 19:28:11 +0100

    [FEAT]: Add footer

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ from dash.dependencies import Input, Output
 from src.components.navbar import create_navbar
 from src.pages.home import layout as home_layout
 from src.pages.about import layout as about_layout
+from src.components.footer import create_footer
 app = Dash(__name__, suppress_callback_exceptions=True, assets_folder="src/assets")
 
 app.layout = html.Div([
     dcc.Location(id='url', refresh=False),
     create_navbar(),
-    html.Div(id='page-content')
+    html.Div(id='page-content'),
+    create_footer()
 ])
 
 @app.callback(
---
+++ src/components/footer.py
@@ -0,0 +1,52 @@
+from dash import html
+
+def create_footer():
+    return html.Footer(
+        className="footer",
+        children=[
+            html.Div(
+                className="footer_top",
+                children=[
+                    html.Img(
+                        className="footer_logo",
+                        src="/assets/images/monogram.png",
+                        alt="Logo"
+                    ),
+                    html.Div(
+                        className="footer_icons",
+                        children=[
+                            html.A(
+                                href="https://github.com/clemence-carcanade/Dashboard_Gender_Gap",
+                                target="_blank",
+                                children=html.Img(className="logo_links", src="/assets/images/github.png", alt="GitHub")
+                            ),
+                            html.A(
+                                href="https://www.figma.com/design/ZsVodzjamAdu6Hm0btIBPQ/DashBoard_Gender_Gap?node-id=0-1&t=52s2Xcpp67KOVo5I-1",
+                                target="_blank",
+                                children=html.Img(className="logo_links", src="/assets/images/figma.png", alt="Figma")
+                            ),
+                        ]
+                    ),
+                    html.Div(
+                        className="footer_links",
+                        children=[
+                            html.Ul([
+                                html.Li(html.B("Thaïs Castillo")),
+                                html.Li("Apprentie Cheffe de Projet - Crédit Agricole"),
+                                html.Li("thais.castillo@edu.esiee.fr"),
+                            ]),
+                            html.Ul([
+                                html.Li(html.B("Clémence Carcanade")),
+                                html.Li("Apprentie Ingénieure Logiciel et IA - Dassault Systèmes"),
+                                html.Li("clemence.carcanade@edu.esiee.fr"),
+                            ])
+                        ]
+                    )
+                ]
+            ),
+            html.Div(
+                "Copyright © 2026 — Designed by Thaïs Castillo & Clémence Carcanade",
+                className="footer_bottom"
+            )
+        ]
+    )
