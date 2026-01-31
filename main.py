95f0b1608b81b9f985558b6071ae192e5d1dfd96
clemence-carcanade <clemence.carcanade@edu.esiee.fr>
2026-02-01 00:25:36 +0100

    [FEAT]: Add favicon and title

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from src.components.navbar import create_navbar
 from src.pages.home import layout as home_layout
 from src.pages.about import layout as about_layout
 from src.components.footer import create_footer
-app = Dash(__name__, suppress_callback_exceptions=True, assets_folder="src/assets")
+app = Dash(__name__, suppress_callback_exceptions=True, assets_folder="src/assets", title="Gender Gap in STEM")
 
 app.layout = html.Div([
     dcc.Location(id='url', refresh=False),
